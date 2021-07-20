51fa79a14ea351ffd40be65ec134c65694a635c9
FEM <fem@fem.ohsu.edu>
2021-07-20 15:10:47 -0700

    upd 1

---
+++ rbin/Classifier_FXs.R
@@ -0,0 +1,29 @@
+
+# classifier_Seurat : calls seurat fx to classify a query dataset 
+
+classifier_Seurat <- function(){
+ 
+  
+  
+  
+  # outLS must contain appropriate output for classify_cells()
+  outLS = list()
+  return(outLS)
+}
+
+
+
+
+
+# classifier_SingleR : calls seurat fx to classify a query dataset 
+
+classifier_SingleR <- function(){
+  
+  
+  
+  
+  # outLS must contain appropriate output for classify_cells()
+  outLS = list()
+  return(outLS)
+}
+
