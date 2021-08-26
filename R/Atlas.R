c4af0792e60781c63bcb31af509326814b14d66d
bbimber <bbimber@gmail.com>
2021-08-25 19:43:23 -0700

    Misc fixes

--- R/Atlas.R
+++ R/Atlas.R
@@ -10,11 +10,14 @@ GetRiraCountMatrix <- function(version = NULL) {
   parentFolder <- .GetAtlasBaseDir(version)
 
   countMat <- paste0(parentFolder, '/counts')
-  countMat <- DropletUtils::read10xCounts(countMat)
-
+  countMat <- DropletUtils::read10xCounts(countMat, col.names = T)
+  countMat2 <- countMat@assays@data$counts
+  rownames(countMat2) <- rownames(countMat)
+  colnames(countMat2) <- colnames(countMat)
+  
   metadata <- paste0(parentFolder, '/meta.csv')
   metadata <- read.csv(metadata, header = T, row.names = 1)
 
-  return(Seurat::CreateSeuratObject(counts = countMat, project = paste0('RIRA-', version), meta.data = metadata))
+  return(Seurat::CreateSeuratObject(counts = countMat2, project = paste0('RIRA-', version), meta.data = metadata))
 }
 
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -4,7 +4,8 @@ test_that("Atlas loading works", {
   mat <- matrix(sample(x = 0:10, size = 100, replace = T), ncol = 10)
   colnames(mat) <- LETTERS[1:10]
   rownames(mat) <- paste0('Gene', LETTERS[1:10])
-
+  mat <- Seurat::as.sparse(mat)
+  
   fn <- './AtlasTemp'
   dir.create(fn)
   fn <- normalizePath(fn)
@@ -15,7 +16,7 @@ test_that("Atlas loading works", {
   fn <- paste0(fn, '/', version)
   dir.create(fn)
 
-  DropletUtils::write10xCounts(mat, path = paste0(fn, '/counts'))
+  DropletUtils::write10xCounts(x = mat, path = paste0(fn, '/counts'), overwrite = T)
 
   meta <- data.frame(Col1 = 1:ncol(mat))
   rownames(meta) <- colnames(mat)
