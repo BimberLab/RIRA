5919519adb17a448369bf0cfc1ee67e226545c09
bbimber <bbimber@gmail.com>
2021-12-17 08:45:27 -0800

    Initial support for scGate and RF (#10)
    * Initial support for scGate and RF dependencies

--- R/Utils.R
+++ R/Utils.R
@@ -1,6 +1,27 @@
 
 pkg.env <- new.env(parent=emptyenv())
 
+pkg.env$RANDOM_SEED <- 1234
+set.seed(pkg.env$RANDOM_SEED)
+
+#' @title Set random seed
+#'
+#' @description Sets the seed used for R‘s random number generator, which should be used in all internal functions
+#' @param seed The random seed
+#' @export
+SetSeed <- function(seed) {
+  pkg.env$RANDOM_SEED <- seed
+  set.seed(pkg.env$RANDOM_SEED)
+}
+
+#' @title Get random seed
+#'
+#' @description Sets a random seed, which should be used in all internal functions
+#' @export
+GetSeed <- function() {
+  return(pkg.env$RANDOM_SEED)
+}
+
 #' @title Set the directory where atlas data reside
 #'
 #' @description Sets the directory where atlas data reside.
