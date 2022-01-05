9e8f52d2cd0de36cca2cbbe3bbe4f59d86841eda
bbimber <bbimber@gmail.com>
2022-01-04 18:08:21 -0800

    Add plots

--- tests/testthat/test-scgate.R
+++ tests/testthat/test-scgate.R
@@ -18,7 +18,20 @@ test_that("scGate Runs", {
   suppressWarnings(SeuratData::InstallData("pbmc3k"))
   suppressWarnings(data("pbmc3k"))
   seuratObj <- suppressWarnings(pbmc3k)
-
+  
+  # Try without reductions present:
+  seuratObj <- RunScGate(seuratObj, gate)
+  expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2343)
+  
+  # Add reductions so plotting will work:
+  seuratObj <- suppressWarnings(pbmc3k)
+  seuratObj <- Seurat::NormalizeData(seuratObj)
+  seuratObj <- Seurat::FindVariableFeatures(seuratObj, nfeatures = 2000)
+  seuratObj <- Seurat::ScaleData(seuratObj)
+  seuratObj <- Seurat::RunPCA(seuratObj, features = Seurat::VariableFeatures(object = seuratObj))
+  seuratObj <- Seurat::FindNeighbors(seuratObj, dims = 1:10)
+  seuratObj <- Seurat::FindClusters(seuratObj, resolution = 0.5)
+  
   seuratObj <- RunScGate(seuratObj, gate)
   expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2343)
 })
