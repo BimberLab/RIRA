6fd8a85b93b2ec6ed1f70e79676d0dc8ebacaffc
bbimber <bbimber@gmail.com>
2022-01-22 10:35:38 -0800

    Update tests

--- tests/testthat/test-scgate.R
+++ tests/testthat/test-scgate.R
@@ -21,7 +21,7 @@ test_that("scGate Runs", {
   
   # Try without reductions present:
   seuratObj <- RunScGate(seuratObj, gate)
-  expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2343)
+  expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2336)
   
   # Add reductions so plotting will work:
   seuratObj <- suppressWarnings(pbmc3k)
@@ -33,7 +33,7 @@ test_that("scGate Runs", {
   seuratObj <- Seurat::FindClusters(seuratObj, resolution = 0.5)
   
   seuratObj <- RunScGate(seuratObj, gate)
-  expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2343)
+  expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2336)
 })
 
 
@@ -58,7 +58,7 @@ test_that("scGates runs on all", {
   dat <- table(seuratObj$scGateConsensus)
   dat
   
-  expect_equal(unname(dat[['Bcell,PanBcell']]), 238)
+  expect_equal(unname(dat[['Bcell,PanBcell']]), 245)
   expect_equal(unname(dat[['NK']]), 151)
 })
 
