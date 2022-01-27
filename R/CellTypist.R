0fe984d77df13bc0b0b5fad23c1a60ff9a06f816
bbimber <bbimber@gmail.com>
2022-01-26 17:37:32 -0800

    Method for celltypist train

--- R/CellTypist.R
+++ R/CellTypist.R
@@ -17,6 +17,7 @@ RunCellTypist <- function(seuratObj, modelName = "Immune_All_Low.pkl") {
     stop('The celltypist python package has not been installed!')
   }
 
+  print(paste0('Running celltypist using model: ', modelName))
   outFile <- tempfile()
   outDir <- dirname(outFile)
   seuratObj <- Seurat::DietSeurat(seuratObj)
@@ -31,7 +32,6 @@ RunCellTypist <- function(seuratObj, modelName = "Immune_All_Low.pkl") {
   # "-m", "celltypist.command_line",
   # "--plot-results"
   args <- c("-i", seuratAnnData, "-m", modelName, "--outdir", outDir, "--majority-voting", "--prefix", "celltypist.", "--quiet")
-  print(args)
   pyOut <- system2(exe, args, stdout = TRUE, stderr = TRUE)
   print(pyOut)
 
@@ -49,4 +49,50 @@ RunCellTypist <- function(seuratObj, modelName = "Immune_All_Low.pkl") {
   return(seuratObj)
 }
 
+#' @title Train Celltypist
+#'
+#' @description Train celltypist using the data in a seurat object and saves the resulting model
+#' @param seuratObj The seurat object
+#' @param labelField The field in seuratObj@meta.data holding the labels for training
+#' @param modelFile The path to save the model
+#'
+#' @export
+TrainCellTypist <- function(seuratObj, labelField, modelFile) {
+  if (!reticulate::py_available(initialize = TRUE)) {
+    stop(paste0('Python/reticulate not configured. Run "reticulate::py_config()" to initialize python'))
+  }
+
+  if (!reticulate::py_module_available('celltypist')) {
+    stop('The celltypist python package has not been installed!')
+  }
+
+  if (!labelField %in% names(seuratObj@meta.data)) {
+    stop('The labelField must be a field in seuratObj@meta.data')
+  }
+
+  modelFile <- gsub(modelFile, pattern = '\\\\', replacement = '/')
+  outFile <- gsub(tempfile(), pattern = '\\\\', replacement = '/')
+  outDir <- dirname(outFile)
+  seuratObj <- Seurat::DietSeurat(seuratObj)
+  trainData <- SeuratToAnnData(seuratObj, paste0(outFile, '-seurat-annData'))
+  labelFile <- paste0(outDir, '.seurat.labels.txt')
+  scriptFile <- paste0(outDir, '.seurat.train.py')
+
+  write.table(seuratObj@meta.data[[labelField]], row.names = F, sep = '\t', quote = F, col.names = F, file = labelFile)
+
+  typistCommand <- c(
+    "import celltypist;",
+    paste0("new_model = celltypist.train('", trainData, "', labels = '", labelFile, "', use_SGD = False, solver = 'saga', feature_selection = True, top_genes = 300);"),
+    paste0("new_model.write('", modelFile, "');")
+  )
+
+  write(typistCommand, file = scriptFile)
+
+  pyOut <- system2(reticulate::py_exe(), c(scriptFile), stdout = TRUE, stderr = TRUE)
+  print(pyOut)
+
+  #unlink(labelFile)
+  #unlink(scriptFile)
+  #unlink(trainData)
+}
 
--- tests/testthat/test-celltypist.R
+++ tests/testthat/test-celltypist.R
@@ -7,10 +7,26 @@ test_that("celltypist runs", {
   suppressWarnings(SeuratData::InstallData("pbmc3k"))
   suppressWarnings(data("pbmc3k"))
   seuratObj <- suppressWarnings(pbmc3k)
-  seuratObj <- Seurat::NormalizeData(seuratObj)
+  seuratObj <- Seurat::NormalizeData(seuratObj, verbose = FALSE)
   
   seuratObj <- RIRA::RunCellTypist(seuratObj)
   
+  expect_equal(9, length(unique(seuratObj$majority_voting)))
+  expect_equal(30, length(unique(seuratObj$predicted_labels)))
+  expect_equal(323, unname(table(seuratObj$predicted_labels)['B cells']))
+
+  modelFile <- 'myModel.pkl'
+  TrainCellTypist(seuratObj, 'majority_voting', paste0(getwd(), '/', modelFile))
+
+  stop()
+  
+  # A bit circular, but this is just a test case:
+  seuratObj <- suppressWarnings(pbmc3k)
+  seuratObj <- Seurat::NormalizeData(seuratObj, verbose = FALSE)
+  seuratObj <- RIRA::RunCellTypist(seuratObj, modelName = modelFile)
+
+  print(table(seuratObj$majority_voting))
+  print(table(seuratObj$predicted_labels))
   expect_equal(9, length(unique(seuratObj$majority_voting)))
   expect_equal(30, length(unique(seuratObj$predicted_labels)))
   expect_equal(323, unname(table(seuratObj$predicted_labels)['B cells']))
