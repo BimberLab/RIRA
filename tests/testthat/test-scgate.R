a4c23eb340975d39a231e2f066577236daaed08b
bbimber <bbimber@gmail.com>
2022-02-01 21:25:13 -0800

    Allow aliasing between model name and the cell label for scGate (#18)
    * Allow aliasing between model name and the cell label for scGate

--- tests/testthat/test-scgate.R
+++ tests/testthat/test-scgate.R
@@ -23,7 +23,17 @@ test_that("scGate Runs", {
   # Try without reductions present:
   seuratObj <- RunScGate(seuratObj, gate)
   expect_equal(sum(seuratObj$is.pure.level1 == 'Pure'), 2336, info = 'Before DimRedux')
-  
+
+  # Try with aliasing of models:
+  seuratObj <- RIRA::RunScGateForModels(seuratObj, modelNames = c('Bcell', 'Tcell', 'NK', 'Myeloid'), labelRename = list(Tcell = 'T_NK', NK = 'T_NK'))
+  print(table(seuratObj$scGateConsensus))
+  dat <- table(seuratObj$scGateConsensus)
+  expect_equal(unname(dat[['Bcell']]), 244, info = 'With aliasing')
+  expect_equal(unname(dat[['T_NK']]), 1601, info = 'With aliasing')
+
+  expect_false('Tcell' %in% names(dat), info = 'With aliasing')
+  expect_false('NK' %in% names(dat), info = 'With aliasing')
+
   # Add reductions so plotting will work:
   seuratObj <- suppressWarnings(pbmc3k)
   seuratObj <- Seurat::NormalizeData(seuratObj)
