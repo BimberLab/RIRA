55bb2ef17a8fc4791dede7d55de7447248b9b2ad
bbimber <bbimber@gmail.com>
2022-08-08 12:11:36 -0700

    Add capability to store gene weights and score seurat object based on them

--- tests/testthat/test-phenotyping.R
+++ tests/testthat/test-phenotyping.R
@@ -1,3 +1,7 @@
+library(Seurat)
+library(SeuratData)
+library(testthat)
+
 context("Phenotyping")
 
 test_that("Gene sets work", {
@@ -8,4 +12,15 @@ test_that("Gene sets work", {
 test_that("ExpandGeneList works", {
   expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A'))), 109)
   expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A', 'MMul10_MHC'))), 144)
+})
+
+test_that("ScoreUsingSavedComponent works", {
+  suppressWarnings(SeuratData::InstallData("pbmc3k"))
+  suppressWarnings(data("pbmc3k"))
+  seuratObj <- suppressWarnings(pbmc3k)
+
+  df <- ExtractGeneWeights(seuratObj, componentNum = 1)
+
+  seuratObj <- ScoreUsingSavedComponent(seuratObj, componentOrName = df, fieldName = 'Test1')
+  seuratObj <- ScoreUsingSavedComponent(seuratObj, componentOrName = 'Tcell_NaiveToEffector', fieldName = 'Test2')
 })
