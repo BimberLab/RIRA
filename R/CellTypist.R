8bbb76ab93ab9c3867a9fd69b2c2d9e21e4a1257
bbimber <bbimber@gmail.com>
2022-09-11 09:08:43 -0700

    Bugfixes

--- R/CellTypist.R
+++ R/CellTypist.R
@@ -69,7 +69,7 @@ RunCellTypist <- function(seuratObj, modelName = "Immune_All_Low.pkl", pThreshol
     toDrop <- grepl(labels$majority_voting, pattern = '\\|')
     if (sum(toDrop) > 0) {
       print(paste0('Converting ', sum(toDrop), ' cells with ambiguous values for majority_voting to NAs'))
-      labels$majority_voting[!is.na(labels$majority_voting) & toDrop] <- NA
+      labels$majority_voting[toDrop] <- NA
     }
   }
 
@@ -83,7 +83,8 @@ RunCellTypist <- function(seuratObj, modelName = "Immune_All_Low.pkl", pThreshol
         print(paste0(toDrop, collapse = ', '))
       }
 
-      labels[fieldName][!is.na(labels[[fieldName]]) & labels[[fieldName]] %in% toDrop] <- NA
+      # NOTE: %in% doesnt handle NAs well
+      labels[fieldName][is.na(labels[[fieldName]]) | labels[[fieldName]] %in% toDrop] <- NA
     }
   }
 
