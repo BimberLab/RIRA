4f7d686572cd01972f1e67230b310eccfc004682
bbimber <bbimber@gmail.com>
2022-10-25 16:00:45 -0700

    Remove TCR Constant region from GetMMul10TcrGenes set (#59)
    * Refine GeneSets to remove TCR constant region from GetMMul10TcrGenes

--- tests/testthat/test-phenotyping.R
+++ tests/testthat/test-phenotyping.R
@@ -5,13 +5,13 @@ library(testthat)
 context("Phenotyping")
 
 test_that("Gene sets work", {
-  expect_equal(length(GetGeneSet('MMul10TcrGenes')), 107)
+  expect_equal(length(GetGeneSet('MMul10TcrGenes')), 102)
   expect_equal(length(GetGeneSet('TandNK_Activation.1')), 8)
 })
 
 test_that("ExpandGeneList works", {
-  expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A'))), 109)
-  expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A', 'MMul10_MHC'))), 144)
+  expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A'))), 104)
+  expect_equal(length(ExpandGeneList(c('MMul10TcrGenes', 'TBX21', 'CD8A', 'MMul10_MHC'))), 139)
 })
 
 test_that("ScoreUsingSavedComponent works", {
