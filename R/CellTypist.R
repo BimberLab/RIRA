b4f1d466d667728744ae00a41742fc5f4506b65e
bbimber <bbimber@gmail.com>
2023-01-12 12:14:37 -0800

    Update methods for default RIRA cell typing

--- R/CellTypist.R
+++ R/CellTypist.R
@@ -381,7 +381,7 @@ Classify_TNK <- function(seuratObj, assayName = 'RNA', columnPrefix = 'RIRA_TNK_
   return(RunCellTypist(seuratObj = seuratObj,
          modelName = "CD4vCD8vGDvNK_v2",
          # These are optimized for this model:
-         pThreshold = 0.5, minProp = 0, useMajorityVoting = FALSE, mode = "best_match",
+         pThreshold = 0.5, minProp = 0, useMajorityVoting = FALSE, mode = "prob_match",
 
          assayName = assayName,
          columnPrefix = columnPrefix,
@@ -391,5 +391,38 @@ Classify_TNK <- function(seuratObj, assayName = 'RNA', columnPrefix = 'RIRA_TNK_
          minCellsToRun = minCellsToRun,
          maxBatchSize = maxBatchSize,
          retainProbabilityMatrix = retainProbabilityMatrix
+  ))
+}
+
+
+#' @title Classify Bulk Immune cells
+#'
+#' @description Runs celltypist using the RIRA bulk immune model to score cells using CellTypist with optimized parameters.
+#' @param seuratObj The seurat object
+#' @param assayName The name of the assay to use. Others will be dropped
+#' @param columnPrefix A prefix that will be added to the beginning of the resulting columns, added the seurat@meta.data
+#' @param convertAmbiguousToNA If true, any values for majority_voting with commas (indicating they are ambiguous) will be converted to NA
+#' @param maxAllowableClasses Celltypist can assign a cell to many classes, creating extremely long labels. Any cell with more than this number of labels will be set to NA
+#' @param minFractionToInclude If non-null, any labels with fewer than this fraction of cells will be set to NA.
+#' @param minCellsToRun If the input seurat object has fewer than this many cells, NAs will be added for all expected columns and celltypist will not be run.
+#' @param maxBatchSize If more than this many cells are in the object, it will be split into batches of this size and run in serial.
+#' @param retainProbabilityMatrix If true, the celltypist probability_matrix with per-class probabilities will be stored in meta.data
+#'
+#' @export
+Classify_ImmuneCells <- function(seuratObj, assayName = 'RNA', columnPrefix = 'RIRA_Immune_v1.', convertAmbiguousToNA = FALSE, maxAllowableClasses = 6, minFractionToInclude = 0.01, minCellsToRun = 200, maxBatchSize = 600000, retainProbabilityMatrix = FALSE) {
+  return(RunCellTypist(seuratObj = seuratObj,
+                       modelName = 'RIRA_Immune_v1',
+
+                       # These are optimized for this model:
+                       minProp = 0.7, useMajorityVoting = TRUE,
+
+                       assayName = assayName,
+                       columnPrefix = columnPrefix,
+                       convertAmbiguousToNA = convertAmbiguousToNA,
+                       maxAllowableClasses = maxAllowableClasses,
+                       minFractionToInclude = minFractionToInclude,
+                       minCellsToRun = minCellsToRun,
+                       maxBatchSize = maxBatchSize,
+                       retainProbabilityMatrix = retainProbabilityMatrix
   ))
 }
