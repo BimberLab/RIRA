01b7cfb574e0cf165a387dad2ff99fc445bc1d18
bbimber <bbimber@gmail.com>
2023-02-27 15:18:54 -0800

    Update args

--- R/CellTypist.R
+++ R/CellTypist.R
@@ -462,11 +462,12 @@ Classify_ImmuneCells <- function(seuratObj, assayName = Seurat::DefaultAssay(seu
 #' @description This is used to flag cells with high UCell score combinations generally indiciative of contamination or doublets
 #' @param seuratObj The seurat object
 #' @param sourceField The name of the field on which to compare
+#' @param outputFieldName The name of the field to store the results
 #' @param ucellCutoff Any cells expressing the disallowed UCell above this value will be flagged
 #' @param disallowedClasses This is a list where the names are the cell classes (which should match levels in sourceField), and values are a vector of UCell field names.
 #'
 #' @export
-FilterDisallowedClasses <- function(seuratObj, sourceField = 'RIRA_Immune_v1.majority_voting', ucellCutoff = 0.2, disallowedClasses = list(
+FilterDisallowedClasses <- function(seuratObj, sourceField = 'RIRA_Immune_v1.majority_voting', outputFieldName = 'FilterStatus', ucellCutoff = 0.2, disallowedClasses = list(
   T_NK = c('Bcell.RM_UCell', 'Myeloid_UCell', 'Erythrocyte.RM_UCell', 'Platelet.RM_UCell', 'NeutrophilLineage.RM_UCell'),
   Myeloid = c('Bcell.RM_UCell', 'Tcell.RM_UCell', 'NK.RM_UCell', 'Erythrocyte.RM_UCell', 'Platelet.RM_UCell', 'NeutrophilLineage.RM_UCell'),
   Bcell = c('Tcell.RM_UCell', 'NK.RM_UCell', 'Myeloid_UCell', 'Erythrocyte.RM_UCell', 'Platelet.RM_UCell', 'NeutrophilLineage.RM_UCell')
@@ -491,12 +492,12 @@ FilterDisallowedClasses <- function(seuratObj, sourceField = 'RIRA_Immune_v1.maj
 
   allCells <- data.frame(cellbarcode = colnames(seuratObj), sortOrder = 1:ncol(seuratObj))
   allCells <- merge(allCells, toDrop, by = 'cellbarcode', all.x = T)
-  allCells <- arrange(allCells, sortOrder)
+  allCells <- dplyr::arrange(allCells, sortOrder)
 
   toAdd <- allCells$reason
   names(toAdd) <- allCells$cellbarcode
 
-  seuratObj <- Seurat::AddMetaData(seuratObj, toAdd)
+  seuratObj <- Seurat::AddMetaData(seuratObj, toAdd, col.name = outputFieldName)
 
   return(seuratObj)
 }
