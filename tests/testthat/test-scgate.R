38a0518f133c48f1007cc24842152c8fa6885621
bbimber <bbimber@gmail.com>
2023-03-23 12:54:19 -0700

    Add gate for Complement.RM

--- tests/testthat/test-scgate.R
+++ tests/testthat/test-scgate.R
@@ -10,7 +10,7 @@ testthat::context("scGate")
 
 test_that("scGates load", {
   gates <- GetAvailableScGates()
-  expect_equal(length(gates), 25)
+  expect_equal(length(gates), 26)
 
   gate <- GetScGateModel('demo_gate')
   expect_equal(length(gate$levels), 59)
