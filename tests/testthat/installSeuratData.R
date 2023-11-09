a5c3a335b3a0bc810c0d364454b8070dcf49201e
bbimber <bbimber@gmail.com>
2023-11-09 09:19:29 -0800

    Add UpdateSeuratObject

--- tests/testthat/installSeuratData.R
+++ tests/testthat/installSeuratData.R
@@ -1,7 +1,7 @@
 getBaseSeuratData <- function(){
   suppressWarnings(SeuratData::InstallData("pbmc3k"))
   suppressWarnings(data("pbmc3k"))
-  seuratObj <- suppressWarnings(pbmc3k)
+  seuratObj <- suppressWarnings(Seurat::UpdateSeuratObject(pbmc3k))
 
   return(seuratObj)
 }
