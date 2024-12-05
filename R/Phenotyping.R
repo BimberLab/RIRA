b0564fa0a72a926efd75bcff5ad826750b2f1d68
bbimber <bbimber@gmail.com>
2024-12-05 10:46:30 -0800

    Add TIMP1 and MIP1a/b isoforms

--- R/Phenotyping.R
+++ R/Phenotyping.R
@@ -49,7 +49,7 @@ PlotImmuneMarkers <- function(seuratObj, reductions = c('tsne', 'umap')) {
 	# CD154 = CD40L
 	# LAMP1 = CD107a, cytotoxic capacity
 	# NR4A1 = Nur77
-	PlotMarkerSeries(seuratObj, reductions = reductions, features = c('IFNG', 'CD69', 'TNF', 'NFKBID', 'LTB', 'CSF2', 'IL2', 'TNFRSF9', 'CCL4L1', 'NR4A3', 'TNFSF14', 'CD82', 'PIGT', 'IRF8', 'IRF4', 'IRF2', 'RGCC', 'PD1', 'PDCD1', 'TNFAIP3', 'LOC100423131', 'LOC100430627', 'ENSMMUG00000013779', 'XCL1', 'ENSMMUG00000060218', 'CCL4', 'CCL3', 'PLEK', 'NR4A2', 'LOC100426537', 'LOC114673087', 'KLF10', 'GADD45B', 'CD154', 'LAMP1', 'NR4A1', 'LOC703222', 'LOC701946', 'LOC100429751', 'LOC100423954'), 'CD8 Activation Markers')
+	PlotMarkerSeries(seuratObj, reductions = reductions, features = c('IFNG', 'CD69', 'TNF', 'NFKBID', 'LTB', 'CSF2', 'IL2', 'TNFRSF9', 'CCL4L1', 'NR4A3', 'TNFSF14', 'CD82', 'PIGT', 'IRF8', 'IRF4', 'IRF2', 'RGCC', 'PD1', 'PDCD1', 'TNFAIP3', 'LOC100423131', 'LOC100430627', 'ENSMMUG00000013779', 'XCL1', 'ENSMMUG00000060218', 'CCL4', 'CCL3', 'PLEK', 'NR4A2', 'LOC100426537', 'LOC114673087', 'KLF10', 'GADD45B', 'CD154', 'LAMP1', 'NR4A1', 'TIMP1', 'LOC703222', 'LOC701946', 'LOC100429751', 'LOC100423954'), 'CD8 Activation Markers')
 
 	# LEF1 = naive
 	# STAT1 = Th1 helper
@@ -322,7 +322,7 @@ GetGeneSet <- function(name) {
 
 .RegisterGeneSet('CD8Memory', c('CD8A', 'CST7', 'CTSW', 'GNLY', 'GZMK'))
 
-.RegisterGeneSet('InterferonAndCytokines', c('IFNA1', 'IFNA2', 'IFNB1', 'IL6', 'IFNG', 'TNF', 'CCL3', 'CCL4'))
+.RegisterGeneSet('InterferonAndCytokines', c('IFNA1', 'IFNA2', 'IFNB1', 'IL6', 'IFNG', 'TNF', 'CCL3', 'CCL3L1', 'CCL4', 'CCL4L1'))
 
 .RegisterGeneSet('Interleukins', c('IL1', 'IL2', 'IL3', 'IL4', 'IL5', 'IL6', 'IL7', 'CXCL8', 'IL9', 'IL10', 'IL11', 'IL13', 'TXLNA', 'IL15', 'IL16', 'IL17A', 'IL17B', 'IL18', 'IL19', 'IL20', 'IL21', 'IL22'))
 
@@ -332,7 +332,7 @@ GetGeneSet <- function(name) {
 # TNFRSF6 = Fas
 .RegisterGeneSet('Apoptosis', c('FASLG', 'FAS', 'BCL2', 'IRF9', 'JAK3', 'CASP3', 'CASP8', 'BAX', 'TNFRSF1A', 'TP53', 'CYCS', 'PARP1'))
 
-.RegisterGeneSet('EffectorCytokines', c('IFNG', 'TNF', 'IL17A', 'IL2', 'CSF2', 'CCL3', 'CCL4'))
+.RegisterGeneSet('EffectorCytokines', c('IFNG', 'TNF', 'IL17A', 'IL2', 'CSF2', 'CCL3', 'CCL3L1', 'CCL4', 'CCL4L1'))
 .RegisterGeneSet('ExhaustionOrInhibitory', c('PDCD1', 'TIGIT', 'HAVCR2', 'LAG3', 'CTLA4', 'VTCN1', 'CD244', 'KLRG1', 'TNFRSF14', 'BTLA', 'CD160'))
 
 .RegisterGeneSet('Myelocytes', c("OLFM4", "LTF", "CAMP", "LCN2"))
