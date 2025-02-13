1e7cbffdc214aaefb5937ceb8858b9103e1611ef
bbimber <bbimber@gmail.com>
2025-02-13 08:37:11 -0800

    Update GEO paths and add test (#128)
    * Update GEO paths and add test
    
    * Test fix

---
+++ tests/testthat/test-geo.R
@@ -0,0 +1,23 @@
+library(Seurat)
+library(SeuratData)
+library(testthat)
+
+context("GEO")
+
+test_that("GEO Files Exist", {
+  expected <- sort(c(
+    "GSE277821_feature_reference.xlsx",
+    "GSE277821_RIRA.All.ADT.counts.rds",
+    "GSE277821_RIRA.All.MergedClonotypes.txt.gz",
+    "GSE277821_RIRA.All.Metadata.rds",
+    "GSE277821_RIRA.All.RNA.counts.rds",
+    "GSE277821_RIRA.All.seurat.rds",
+    "GSE277821_RIRA.Bcell.seurat.rds",
+    "GSE277821_RIRA.Myeloid.seurat.rds",
+    "GSE277821_RIRA.T_NK.seurat.rds"
+  ))
+
+  fn <- RCurl::getURL('ftp.ncbi.nlm.nih.gov/geo/series/GSE277nnn/GSE277821/suppl/', .opts = RCurl::curlOptions(ftplistonly=TRUE))
+  fn <- sort(gsub(unlist(strsplit(fn, split = '\n')), pattern = '\r', replacement = ''))
+  expect_equal(fn, expected)
+})
