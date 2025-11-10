2312d13f18109341f97a73cbb419ebf69d38527d
bbimber <bbimber@gmail.com>
2025-11-10 05:49:24 -0800

    Test fix

--- tests/testthat/test-phenotyping.R
+++ tests/testthat/test-phenotyping.R
@@ -26,8 +26,8 @@ test_that("ScoreUsingSavedComponent works", {
   expect_equal(nrow(df), 200)
 
   seuratObj <- ScoreUsingSavedComponent(seuratObj, componentOrName = df, fieldName = 'Test1')
-  expect_equal(round(min(seuratObj$Test1), 2), -2.52)
-  expect_equal(round(max(seuratObj$Test1), 2), 199.21)
+  expect_equal(round(min(seuratObj$Test1), 2), -199)
+  expect_equal(round(max(seuratObj$Test1), 2), 2.52)
   
   seuratObj <- ScoreUsingSavedComponent(seuratObj, componentOrName = 'Tcell_NaiveToEffector', fieldName = 'Test2')
   expect_equal(round(max(seuratObj$Test2), 2), 41.61)
