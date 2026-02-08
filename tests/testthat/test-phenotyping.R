df7df53836f1cf4f2dc7bc8e137060fa3491ceaf
bbimber <bbimber@gmail.com>
2026-02-08 11:04:47 -0800

    Test fix

--- tests/testthat/test-phenotyping.R
+++ tests/testthat/test-phenotyping.R
@@ -32,8 +32,8 @@ test_that("ScoreUsingSavedComponent works", {
   expect_equal(round(max(abs(seuratObj$Test1)), 2), 199.21)
   
   seuratObj <- ScoreUsingSavedComponent(seuratObj, componentOrName = 'Tcell_EffectorDifferentiation', fieldName = 'Test2')
-  expect_equal(round(max(seuratObj$Test2), 2), 41.61)
-  expect_equal(round(min(seuratObj$Test2), 2), -6.69)
+  expect_equal(round(max(seuratObj$Test2), 2), 52.1)
+  expect_equal(round(min(seuratObj$Test2), 2), -6.12)
 
   seuratObj <- CalculateUCellScores(seuratObj)
   expect_true('TandNK_Activation_UCell' %in% names(seuratObj@meta.data))
