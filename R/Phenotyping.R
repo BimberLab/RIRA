ad906f5410d03c5d4420e8aed20741f378e755f6
bbimber <bbimber@gmail.com>
2026-03-23 13:23:00 -0700

    Add CD38

--- R/Phenotyping.R
+++ R/Phenotyping.R
@@ -54,7 +54,7 @@ PlotImmuneMarkers <- function(seuratObj, reductions = c('tsne', 'umap')) {
 	# CD154 = CD40L
 	# LAMP1 = CD107a, cytotoxic capacity
 	# NR4A1 = Nur77
-	PlotMarkerSeries(seuratObj, reductions = reductions, features = c('IFNG', 'CD69', 'TNF', 'NFKBID', 'LTB', 'CSF1', 'CSF2', 'CSF3', 'IL2', 'TNFRSF9', 'CCL4L1', 'NR4A3', 'TNFSF14', 'CD82', 'PIGT', 'IRF8', 'IRF4', 'IRF2', 'RGCC', 'PD1', 'PDCD1', 'TNFAIP3', 'LOC100423131', 'LOC100430627', 'ENSMMUG00000013779', 'XCL1', 'ENSMMUG00000060218', 'CCL4', 'CCL3', 'PLEK', 'NR4A2', 'LOC100426537', 'LOC114673087', 'KLF10', 'GADD45B', 'CD154', 'LAMP1', 'NR4A1', 'TIMP1', 'LOC703222', 'LOC701946', 'LOC100429751', 'LOC100423954'), 'CD8 Activation Markers')
+	PlotMarkerSeries(seuratObj, reductions = reductions, features = c('IFNG', 'CD69', 'TNF', 'NFKBID', 'LTB', 'CSF1', 'CSF2', 'CSF3', 'IL2', 'TNFRSF9', 'CCL4L1', 'NR4A3', 'TNFSF14', 'CD82', 'CD38', 'PIGT', 'IRF8', 'IRF4', 'IRF2', 'RGCC', 'PD1', 'PDCD1', 'TNFAIP3', 'LOC100423131', 'LOC100430627', 'ENSMMUG00000013779', 'XCL1', 'ENSMMUG00000060218', 'CCL4', 'CCL3', 'PLEK', 'NR4A2', 'LOC100426537', 'LOC114673087', 'KLF10', 'GADD45B', 'CD154', 'LAMP1', 'NR4A1', 'TIMP1', 'LOC703222', 'LOC701946', 'LOC100429751', 'LOC100423954'), 'CD8 Activation Markers')
 
 	# LEF1 = naive
 	# STAT1 = Th1 helper
@@ -105,7 +105,7 @@ PlotImmuneMarkers <- function(seuratObj, reductions = c('tsne', 'umap')) {
 
 	PlotMarkerSeries(seuratObj, reductions = reductions, features = c('HAVCR2'), 'Th1')
 
-	PlotMarkerSeries(seuratObj, reductions = reductions, features = c(GetGeneSet('TNK_MaybeExhaustion'), 'CD44', 'AHNAK', 'CX3CR1'), 'T/NK: Possibly Antigen-Exposure/Exhaustion')
+	PlotMarkerSeries(seuratObj, reductions = reductions, features = c(GetGeneSet('TNK_MaybeExhaustion'), 'CD44', 'AHNAK', 'CX3CR1', 'CD38'), 'T/NK: Possibly Antigen-Exposure/Exhaustion')
 
 	# ZBTB16 = PLZF
 	PlotMarkerSeries(seuratObj, reductions = reductions, features = GetGeneSet('MAIT_Markers'), 'MAIT')
